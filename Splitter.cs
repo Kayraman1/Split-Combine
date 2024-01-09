38acb5fa69d322a6e69b1c9380ac35ce0d626953
Kayraman1 <108055142+Kayraman1@users.noreply.github.com>
2024-01-09 16:08:12 +0100

    Splitter

---
+++ Splitter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+class FileSplitter
+{
+    static void Main()
+    {
+        Console.Write("FP: ");
+        string filePath = Console.ReadLine();
+
+        Console.Write("Output: ");
+        string outputDirectory = Console.ReadLine();
+
+        SplitFile(filePath, outputDirectory);
+    }
+
+    static void SplitFile(string filePath, string outputDirectory)
+    {
+        const int chunkSize = 25 * 1024 * 1024; // 25 MB
+        byte[] buffer = new byte[chunkSize];
+
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            int bytesRead;
+            int fileNumber = 1;
+
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string outputFilePath = Path.Combine(outputDirectory, $"part_{fileNumber}.bin");
+                using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    outputFileStream.Write(buffer, 0, bytesRead);
+                }
+
+                fileNumber++;
+            }
+        }
+
+        Console.WriteLine("Fertig");
+    }
+}
